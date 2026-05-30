8f2d348bf1ee5f1dd00200a8189935b11924b7fb
Anson Chung <101555231+thachung@users.noreply.github.com.>
2026-05-30 14:43:24 +1000

    Update to Lean 4 v4.30.0

--- lakefile.lean
+++ lakefile.lean
@@ -5,10 +5,10 @@ package «DeGiorgi» where
   leanOptions := #[⟨`autoImplicit, false⟩]
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4" @ "v4.29.0-rc6"
+  "https://github.com/leanprover-community/mathlib4" @ "v4.30.0"
 
 require REPL from git
-  "https://github.com/leanprover-community/repl" @ "v4.29.0-rc6"
+  "https://github.com/leanprover-community/repl" @ "v4.30.0"
 
 @[default_target]
 lean_lib «DeGiorgi» where
